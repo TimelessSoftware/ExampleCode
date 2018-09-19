aeb4b7e90fbb3ecbd076d428c302ac5ef53ac16e
Christopher Burnett <chris.burnett@lloydsbanking.com>
2018-09-19 23:47:57 +0100

    Initial Commit

---
+++ GitHub Example/GitHub Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Example
+//
+//  Created by Christopher Burnett on 19/09/2018.
+//  Copyright © 2018 Timeless Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GitHub Example/GitHub ExampleTests/GitHub_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_ExampleTests.swift
+//  GitHub ExampleTests
+//
+//  Created by Christopher Burnett on 19/09/2018.
+//  Copyright © 2018 Timeless Software. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Example
+
+class GitHub_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
