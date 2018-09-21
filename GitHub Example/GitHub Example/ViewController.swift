7a864be8eecb81aba85ecc436c5130399d522703
Christopher Burnett <chris.burnett@lloydsbanking.com>
2018-09-21 19:44:15 +0100

    test

--- GitHub Example/GitHub Example/ViewController.swift
+++ GitHub Example/GitHub Example/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //this is an example
+        
+        // set diag as ui & l
     }
 
 
